54e353aec314dd7b8115b5c79cc0714182223516
jherrera245 <245jh17@gmail.com>
2023-04-21 12:57:00 -0600

    Cliente API PHP para app android

---
+++ api.php
@@ -0,0 +1,82 @@
+<?php
+    require_once "config.php";
+
+    //respuesta
+    $datos = array();
+
+    //Estructura de la tabla tareas
+    $columnas = [
+        "id" => NULL,
+        "nombre" => NULL,
+        "descripcion" => NULL,
+        "fecha"=>NULL,
+        "hora"=>NULL,
+        "status"=>NULL,
+    ];
+    
+    $accion = "";
+
+    if (isset($_POST["accion"])) {
+        $accion = $_POST["accion"];
+    }
+
+    //Insertar datos
+    if ($accion == "insertar") {
+        //guardando datos en el arreglo
+        $columnas["nombre"] = $_POST["nombre"];
+        $columnas["descripcion"] = $_POST["descripcion"];
+        $columnas["fecha"] = $_POST["fecha"];
+        $columnas["hora"] = $_POST["hora"];
+
+        if (insertar($columnas) == true) {
+            $datos["estado"] = true;
+            $datos["resultado"] = "Tarea agregada correctamente";
+        }else {
+            $datos["estado"] = false;
+            $datos["resultado"] = "No se pudo agregar la nueva tarea";
+        }
+
+    }
+    
+    //listar las tareas
+    if ($accion == "listar") {
+        $filtro = (isset($_POST["filtro"])) ? $_POST["filtro"] : 0;
+        $datos["estado"] = true;
+        $datos["resultado"] = listar($filtro);
+    }
+
+    
+    //actualiza los datos de la tarea
+    if ($accion == "actualizar") {
+
+        $columnas["id"] = $_POST["id"];
+        $columnas["nombre"] = $_POST["nombre"];
+        $columnas["descripcion"] = $_POST["descripcion"];
+        $columnas["fecha"] = $_POST["fecha"];
+        $columnas["hora"] = $_POST["hora"];
+        $columnas["status"] = $_POST["status"];
+
+        if (actualizar($columnas) == true) {
+            $datos["estado"] = true;
+            $datos["resultado"] = "Tarea actualizada correctamente";
+        }else {
+            $datos["estado"] = false;
+            $datos["resultado"] = "No se pudo actualizar la tarea";
+        }
+    }
+    
+    //elimina la tarea
+    if ($accion == "eliminar") {
+        $id = $_POST["id"];
+
+        if (eliminar($id) == true) {
+            $datos["estado"] = true;
+            $datos["resultado"] = "Tarea eliminada correctamente";
+        }else {
+            $datos["estado"] = false;
+            $datos["resultado"] = "No se pudo eliminar la tarea";
+        }
+    } 
+
+    echo json_encode($datos);
+?> 
---
+++ config.php
@@ -0,0 +1,110 @@
+<?php
+
+#funcion para la conexion a base de datos
+function conectar() {
+    $server = "localhost";
+    $database = "db_todolist";
+    $user = "root";
+    $password = "";
+    // Create connection
+    $conexion = mysqli_connect($server, $user, $password, $database);
+    $conexion->set_charset("utf8mb4");
+
+    // Check connection
+    if (!$conexion) {
+       $conexion = mysqli_error($conexion);
+    }
+
+    return $conexion;
+}
+
+# Funcion para deconectar
+function desconectar($conexion) {
+    try {
+        mysqli_close($conexion);
+        $estado = true;
+    } catch (Exception $e) {
+        $estado = false;
+    }
+    return $estado;
+}
+
+# funcion para insertar datos a la DB
+function insertar($tarea) {
+    
+    $conexion = conectar();
+    
+    $query = "
+        INSERT INTO tareas (id, nombre, descripcion, fecha, hora, status) 
+        VALUES(NULL, '{$tarea["nombre"]}', '{$tarea["descripcion"]}', '{$tarea["fecha"]}', '{$tarea["hora"]}', 0)
+    ";   
+    
+    if (mysqli_query($conexion, $query)) {
+        $estado = true; 
+    }else {
+        $estado = false;
+    }
+
+    desconectar($conexion);
+    
+    return $estado;
+}
+
+# funcion para actualizar los datos de la DB
+function actualizar($tarea) {
+    $conexion = conectar();
+    $query = "
+        UPDATE tareas SET nombre = '{$tarea["nombre"]}', descripcion='{$tarea["descripcion"]}', fecha = '{$tarea["fecha"]}', 
+        hora = '{$tarea["hora"]}', status = {$tarea["status"]} WHERE id = {$tarea["id"]}
+    ";   
+    
+    if (mysqli_query($conexion, $query)) {
+        $estado = true; 
+    }else {
+        $estado = false;
+    }
+
+    desconectar($conexion);
+    return $estado;
+}
+
+# funcion para actualizar datos a la base de datos
+function eliminar($id) {
+    $conexion = conectar();
+    $query = "DELETE FROM tareas WHERE id = '$id'";   
+    
+    if (mysqli_query($conexion, $query)) {
+        $estado = true; 
+    }else {
+        $estado = false;
+    }
+
+    desconectar($conexion);
+    return $estado;
+}
+
+# funcion para listar tareas
+function listar($filtro) {
+    $conexion = conectar();
+    $json = array();
+    $param = ($filtro == 0 || $filtro == 1) ?  "WHERE status = $filtro" : "";
+    $query = "SELECT * FROM tareas  $param";
+    
+    $result = mysqli_query($conexion, $query);
+    
+    if (mysqli_num_rows($result) > 0) {
+        while ($tarea = mysqli_fetch_array($result)) {
+            $row = array();
+            $row['id'] = $tarea['id'];
+            $row['nombre'] = $tarea['nombre'];
+            $row['descripcion'] = $tarea['descripcion'];
+            $row['fecha'] = $tarea['fecha'];
+            $row['hora'] = $tarea['hora'];
+            $row['status'] = $tarea['status'];
+            $json[] = $row;
+        }
+    }
+    
+    desconectar($conexion);
+    return array_values($json);
+}
